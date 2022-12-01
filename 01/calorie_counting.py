18d85778ab67627610c8b457f500b88bf02272fd
skaphle <skaphle@pm.me>
2022-12-01 16:24:36 +0100

    Add solution for second part of puzzle 01

--- 01/calorie_counting.py
+++ 01/calorie_counting.py
@@ -3,6 +3,7 @@
 
 if __name__ == '__main__':
 
+    # part 1: calories of the most loaded elf
     elves = []
     calories = []
     with open('input.txt', encoding='utf-8') as f_in:
@@ -20,3 +21,7 @@ if __name__ == '__main__':
     sum_calories = [sum(elf) for elf in elves]
 
     print(max(sum_calories))
+
+    # part 2: calories of the top 3 most carrying elves
+    sum_calories.sort(reverse=True)
+    print(sum(sum_calories[:3]))
