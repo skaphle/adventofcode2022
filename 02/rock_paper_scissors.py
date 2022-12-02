ec0217be6a8a0181cc6208801f34b58daeb85b1a
skaphle <skaphle@pm.me>
2022-12-02 14:26:17 +0100

    Add solution for second part of puzzle 02

--- 02/rock_paper_scissors.py
+++ 02/rock_paper_scissors.py
@@ -5,7 +5,7 @@
 def calculate_score(other, own):
     if other not in ['A', 'B', 'C'] or own not in ['X', 'Y', 'Z']:
         raise ValueError
-    ord_other = ord(other)  # ord('A')=65, ord('Z')=88
+    ord_other = ord(other)  # ord('A')=65, ord('X')=88
     ord_own = ord(own)
     if ord_other + 23 in (ord_own + 1, ord_own - 2):
         outcome = 0
@@ -14,14 +14,36 @@ def calculate_score(other, own):
     elif ord_other + 23 in (ord_own - 1, ord_own + 2):
         outcome = 6
     shape = ord_own - 87
-    return outcome+shape
+    return outcome + shape
+
+
+def calculate_score_2(other, end):
+    if other not in ['A', 'B', 'C'] or end not in ['X', 'Y', 'Z']:
+        raise ValueError
+    # note that ord('A')=65, ord('X')=88
+    num_end = (ord(end) + 2) % 3  # (X,Y,Z)->(0,1,2)
+    outcome = num_end * 3  # (X,Y,Z)->(0,3,6)
+    shape = (ord(other) + ord(end) + 2) % 3 + 1  # A+(X,Y,Z)->(S,R,P)->3,1,2
+    return outcome + shape
 
 
 if __name__ == '__main__':
 
+    # test cases
+    assert calculate_score('A', 'Y') == 8, "Expected 8"
+    assert calculate_score('B', 'X') == 1, "Expected 1"
+    assert calculate_score('C', 'Z') == 6, "Expected 6"
+
+    assert calculate_score_2('A', 'Y') == 4, "Expected 4"
+    assert calculate_score_2('B', 'X') == 1, "Expected 1"
+    assert calculate_score_2('C', 'Z') == 7, "Expected 7"
+
     total_score = 0
+    total_score_2 = 0
     with open('input.txt', encoding='utf-8') as f_in:
         for line in f_in:
             words = line.split()
             total_score += calculate_score(*words)
+            total_score_2 += calculate_score_2(*words)
     print(f'total_score: {total_score}')
+    print(f'total_score_2: {total_score_2}')
