291cf59a324cc8c1ed4134e0dba11aba788ee861
skaphle <skaphle@pm.me>
2022-12-09 20:25:00 +0100

    Add solution for part two of puzzle 09

--- 09/rope_bridge.py
+++ 09/rope_bridge.py
@@ -5,26 +5,29 @@
 import numpy as np
 
 
-def get_tail_path(instr: str) -> list:
-    H = np.array((0, 0))
-    T = np.array((0, 0))
-    path = [H]
+def get_tail_path(instr: str, knots: int = 2) -> list:
+    rope = [np.array((0,0)) for i in range(knots)]
+    path = [np.array((0,0))]
     for motion in instr.splitlines():
         direction = motion.split()[0]
         steps = int(motion.split()[1])
         if direction == 'R':
-            T_move = np.array((0, 1))
+            head_move = np.array((0, 1))
         elif direction == 'L':
-            T_move = np.array((0, -1))
+            head_move = np.array((0, -1))
         elif direction == 'U':
-            T_move = np.array((1, 0))
+            head_move = np.array((1, 0))
         else:
-            T_move = np.array((-1, 0))
+            head_move = np.array((-1, 0))
         for i in range(steps):
-            T = T + T_move
-            if max(abs(T-H)) > 1:
-                H = H + np.sign(T-H)*(T != H)
-                path.append(H)
+            rope[0] = rope[0] + head_move
+            for j in range(1, knots):
+                knot = rope[j]
+                prev_knot = rope[j-1]
+                if max(abs(prev_knot-knot)) > 1:
+                    knot = knot + np.sign(prev_knot-knot)*(prev_knot != knot)
+                    rope[j] = knot
+            path.append(rope[-1])
     return path
 
 
@@ -44,7 +47,7 @@ if __name__ == '__main__':
     # - change head's position
     # - if distant, change tail and append to list
 
-    # test cases:
+    # tests
     EXAMPLE_1 = """R 4
 U 4
 L 3
@@ -53,7 +56,6 @@ R 4
 D 1
 L 5
 R 2"""
-
     test_path = get_tail_path(EXAMPLE_1)
     # now how to test this?
     # print(f'path: {path}')
@@ -66,3 +68,30 @@ R 2"""
     path_1 = get_tail_path(instructions)
     unique_positions_1 = len(set(tuple(p) for p in path_1))
     print(f'Positions visited at least once: {unique_positions_1}')
+
+    # part two: rope is 10 knots instead of 2
+    #
+    # strategy:
+    # - add intermediate positions, 1 extra loop
+
+    # tests
+    test_path_2 = get_tail_path(EXAMPLE_1, 10)
+    test_unique_positions_2 = len(set(tuple(p) for p in test_path_2))
+    assert test_unique_positions_2 == 1, 'Expected 1 position in test 2'
+    EXAMPLE_2 = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+"""
+    test_path_3 = get_tail_path(EXAMPLE_2, 10)
+    test_unique_positions_3 = len(set(tuple(p) for p in test_path_3))
+    assert test_unique_positions_3 == 36, 'Expected 36 positions in test 3'
+
+    # apply to actual input
+    path_2 = get_tail_path(instructions, 10)
+    unique_positions_2 = len(set(tuple(p) for p in path_2))
+    print(f'Positions visited at least once: {unique_positions_2}')
