3b2efb8ee4c7997fe5572fdfd36f342b6546d2f8
skaphle <skaphle@pm.me>
2022-12-12 16:38:59 +0100

    Add solution for part two of puzzle 12

--- 12/hill_climbing_algorith.py
+++ 12/hill_climbing_algorith.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 
-def compute_distances(input_map: np.array) -> np.array:
+def compute_distances(input_map: np.array, reverse=False) -> np.array:
     M, N = input_map.shape
     t_S = divmod(list(input_map.flat).index('S'), N)
     t_E = divmod(list(input_map.flat).index('E'), N)
@@ -19,21 +19,22 @@ def compute_distances(input_map: np.array) -> np.array:
     height[t_E] = ord('z')
 
     # main loop
-    current = S
+    current = E if reverse else S
     t_c = tuple(current)
-    distance[t_S] = 0
+    distance[t_c] = 0
     for i in range(M*N):
         # check every unvisited, reachable neighbor and note its distance
         for delta in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
             neighbor = current + np.array(delta)
             t_n = tuple(neighbor)
-            if 0 <= neighbor[0] < M and 0 <= neighbor[1] < N and height[t_n] <= height[t_c] + 1:
-                # technically in our setup, the first value is always the minimum
-                distance[t_n] = min(distance[t_n], distance[t_c]+1)
+            if 0 <= neighbor[0] < M and 0 <= neighbor[1] < N:
+                if (reverse and height[t_n] + 1 >= height[t_c] or
+                        not reverse and height[t_n] <= height[t_c] + 1):
+                    # technically in our setup, the first value is always the minimum
+                    distance[t_n] = min(distance[t_n], distance[t_c]+1)
         visited[t_c] = True
         # where to check next?
-        if visited[t_E] or min(input_map[~visited]) == M*N:
-            print(f'Distance to E: {distance[t_E]}')
+        if not reverse and visited[t_E] or len(distance[~visited]) == 0 or min(distance[~visited]) == M*N:
             break
         min_dist = min(distance[~visited])
         t_c = divmod(list(np.where(~visited, distance, -1).flat).index(min_dist), N)
@@ -80,6 +81,27 @@ abdefghi"""
     with open('input.txt', encoding='utf-8') as f_in:
         in_txt = f_in.read()
     map_ar = np.array([list(line) for line in in_txt.splitlines()])
-    endpoint = divmod(list(test_map_ar.flat).index('E'), test_map_ar.shape[1])
+    endpoint = divmod(list(map_ar.flat).index('E'), map_ar.shape[1])
     distance = compute_distances(map_ar)
-    # print(distance[endpoint])
+    print(f'Minimum distance from S to E: {distance[endpoint]}')
+
+    # part 2: fewest steps required from any start at same elevation a
+    #
+    # strategy:
+    # - revert E and S
+    # - continue until whole map is calculated
+    # - take min of options
+    #
+    # modify function to work for both tasks
+    # - create reverse parameter
+
+    # Example
+    test_distance_2 = compute_distances(test_map_ar, reverse=True)
+    test_min_dist = min(test_distance_2[np.where(test_map_ar == 'a')])
+    assert test_min_dist == 29, 'Shortest path in example should be 29'
+
+    # Actual puzzle input
+    distance_2 = compute_distances(map_ar, reverse=True)
+    # ignore S because this was computed in part one
+    min_dist = min(distance_2[np.where(map_ar == 'a')])
+    print(f'Distance in shortest path from any a to E: {min_dist}')
